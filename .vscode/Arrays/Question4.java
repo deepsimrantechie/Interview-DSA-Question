29b66e61b729b38cdaacfa93be99a136411d0df8
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2025-02-03 00:57:39 +0530

    Array question 4 added

--- .vscode/Arrays/Question4.java
+++ .vscode/Arrays/Question4.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        //here we are checking if the array is already empty
-        if(nums.length == 0){
-            return 0;
-        }
+    public boolean canJump(int[] nums) {
 
-        //intializing the empty error for the new array to remove duplicate
-
-        int j=1;
-        //using for loop for continoues iteration
-        for(int i=1; i<nums.length;i++){
-            //checking of the element at first is equal to the previous element or not 
-            if(nums[i] != nums[i-1]){
-                nums[j] = nums[i];
-                j++;
-            }
+        //intializing the farthest with the zero
+        int farthest = 0;
+        //we are using for loop for iteratint the array
+       for (int i=0; i<nums.length;i++){
+        //checking if already the i is greater than the farthest then it is not possible to jump 
+        if(i>farthest){
+            return false;
         }
-        return j;
+//function to find the max from the both the farthest and choosing maximum one 
+        farthest = Math.max(farthest,i+ nums[i]);
+        if(farthest>= nums.length-1){
+            return true;
+        }
+       }
+       return false;
         
     }
 }
