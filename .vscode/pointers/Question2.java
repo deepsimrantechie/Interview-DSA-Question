d0cb0912b56735aa52ff1f217370375222644178
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2025-02-07 23:46:51 +0530

    question added

--- .vscode/pointers/Question2.java
+++ .vscode/pointers/Question2.java
@@ -1,6 +1,3 @@
-///to check either a number is plaindrome or not 
-
-
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]","");
@@ -17,21 +14,3 @@ class Solution {
         return true;
     }
 }
-
-
-
-
-
-
-
-
-
-//first example
-{/**the 
-step 1:race a car //remove all the space from it or all the sign and everything
-step 2: it will be like raceacar a complete without the space between them.
-step 3: now we will check with left and right if left is smaller than right then it will check the 
-step 4: check the if condition
-step 5: it will answer in true or false
-
- */}
